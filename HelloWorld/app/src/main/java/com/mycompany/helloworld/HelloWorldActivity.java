6280cebaf4196c48b0c22fb0cdafc96b14632e43
jingsam <jingsam@163.com>
2016-08-06 18:02:49 +0800

    HelloWorld

---
+++ HelloWorld/app/src/main/java/com/mycompany/helloworld/HelloWorldActivity.java
@@ -0,0 +1,15 @@
+package com.mycompany.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class HelloWorldActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.hello_world_layout);
+        Log.d("HelloWorldActivity", "onCreate execute");
+    }
+}
