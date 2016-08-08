b2424a07b901904b566a4365e5547bb36a66d674
jingsam <jingsam@163.com>
2016-08-08 23:26:18 +0800

    UIWidgetTest

---
+++ UIWidgetTest/app/src/main/java/com/mycompany/uiwidgettest/MainActivity.java
@@ -0,0 +1,52 @@
+package com.mycompany.uiwidgettest;
+
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button button;
+
+    private EditText editText;
+
+    private ImageView imageView;
+
+    private ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(this);
+
+        editText = (EditText) findViewById(R.id.edit_text);
+        imageView = (ImageView) findViewById(R.id.image_view);
+        progressBar = (ProgressBar) findViewById(R.id.progress_bar);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.button:
+                ProgressDialog progressDialog = new ProgressDialog(this);
+                progressDialog.setTitle("THis is ProgressDialog");
+                progressDialog.setMessage("Loading...");
+                progressDialog.setCancelable(true);
+                progressDialog.show();
+                break;
+            default:
+                break;
+        }
+    }
+}
